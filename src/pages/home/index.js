421e7bc8af31924f770aa24245b190f64c4f4efd
tzcacm <313694848@qq.com>
2019-11-15 18:02:56 +0800

    每日一更

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import './index.less';
+import { Icon, Card, Avatar } from 'antd';
+const { Meta } = Card;
 
-function HomePage() {
+const HomePage = () => {
+
+    const homeMenuList = [
+        { icon: 'user', title: '用户总数', count: Math.round(Math.random() * 10000) },
+        { icon: 'user', title: '商品总数', count: Math.round(Math.random() * 1000) },
+        { icon: 'user', title: '订单总数', count: Math.round(Math.random() * 100) },
+        { icon: 'user', title: '浏览总数', count: Math.round(Math.random() * 1000) }
+    ];
 
     return (
-        <div>我是首页内容</div>
+        <div>
+            <div className="home_menu">
+                {
+                    homeMenuList.map((item, index) => {
+                        return (
+                            <div className="menu_list" key={index}>
+                                <Icon type={item['icon']} className="menu_icon"></Icon>
+                                <div className="menu_box">
+                                    <div className="menu_title">{item['title']}</div>
+                                    <div className="menu_number">{item.count}</div>
+                                </div>
+                            </div>
+                        )
+                    })
+
+                }
+            </div>
+
+            <div className="home_card">
+                <Card
+                    hoverable
+                    cover={<img alt="example" src="http://bimgs.plmeizi.com/images/bing/2018/WhiteTiger_ZH-CN12326957209_1920x1080.jpg" />}
+                >
+                    <Meta avatar={<Avatar src='https://gss1.bdstatic.com/9vo3dSag_xI4khGkpoWK1HF6hhy/baike/s%3D220/sign=f23b0796b74543a9f11bfdce2e178a7b/8b13632762d0f703d0ad4cbe08fa513d2697c5b1.jpg' />} title="孟加拉白虎" description="尽管在野外有孟加拉白虎出没的记载，但它们非常罕见。这可能是因为孟加拉白虎的毛皮里缺少色素，这种天生的缺陷降低了它们在自然界的存活率。如今，人们看到的孟加拉白虎都是动物园的园宝。孟加拉白虎在许多文化中具有很强的神话色彩，包括在韩国，它是力量和信任的象征。因此韩国奥林匹克委员会选择了孟加拉白虎作为2018年冬季奥运会的吉祥物之一。" />
+                </Card>
+            </div>
+        </div>
     )
 }
 
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Redirect } from 'react-router-dom';
 import Layout from '../../componets/layout';
 import Header from '../../componets/header';
+import { connect } from 'react-redux';
 import './index.less';
 
-class IndexPage extends Component {
+const IndexPage = (props) => {
 
-    render() {
-        // 监听路由
-        if (!localStorage.getItem('tokenId')) {
-            return <Redirect to="/login"></Redirect>
-        } else {
-            return (
-                <div className="main_box">
-                    <div><Layout></Layout></div>
-                    <div className="main_container">
-                        <Header></Header>
-                        <div className="main_content">
-                            {this.props.children}
-                        </div>
+    if (!localStorage.getItem('tokenId')) {
+        return <Redirect to="/login"></Redirect>
+    } else {
+        return (
+            <div className="main_box">
+                <div className="main_layout"><Layout></Layout></div>
+                <div className="main_container" style={{ marginLeft: props.collapsed ? '80px' : '250px' }}>
+                    <Header></Header>
+                    <div className="main_content">
+                        {props.children}
                     </div>
                 </div>
-            )
-        }
+            </div>
+        )
     }
-
-
 }
 
+const mapStateToProps = (state) => ({
+    collapsed: state.header.collapsed
+})
 
-export default IndexPage
+
+export default connect(mapStateToProps, null)(IndexPage);
