621a1df568657d7b8f849c565e2e0fcae835ee15
tzcacm <313694848@qq.com>
2019-11-19 15:22:31 +0800

    每日一更

--- src/pages/product/components/editInfo/index.js
+++ src/pages/product/components/editInfo/index.js
@@ -1,57 +1,57 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import { Input, Button, Radio } from 'antd';
 import PropTypes from 'prop-types';
 
-const EditInfo = (props) => {
 
-    return (
-        <div className="product_amendbox" style={{ display: props.isShowChange ? 'block' : 'none' }}>
-            {
-                props.editType === 'edit' ?
+class EditInfo extends Component {
+
+    //监听name、price、radio的值
+    dataChange(value, type) {
+        let editData = {};
+        editData[type] = value;
+        const editInfoData = JSON.parse(JSON.stringify(this.props.infoData));
+        const editInfo = Object.assign(editInfoData, editData);
+        this.props.editDataChange(editInfo);
+    }
+
+    render() {
+
+        let { isShowChange, editType, infoData, changeSure, changeCancel } = this.props;
+
+        return (
+            <div className="product_amendbox" style={{ display: isShowChange ? 'block' : 'none' }}>
+                {
                     <div className="product_form">
-                        <div className="product_amendTitle">{props.editInfoData['name']}</div>
+                        {editType === 'edit' ?
+                            <div className="product_amendTitle">{infoData['name']}</div>
+                            :
+                            <div className="product_amendInput">
+                                <label htmlFor="name" className="product_type">商品信息：</label>
+                                <Input placeholder="name" id="name" value={infoData['name']} onChange={(e) => this.dataChange(e.target.value, 'name')} />
+                            </div>
+                        }
                         <div className="product_amendInput">
                             <label htmlFor="price" className="product_type">商品价格：</label>
-                            <Input placeholder="price" id="price" value={props.editInfoData['price']} onChange={props.priceChange} />
+                            <Input placeholder="price" id="price" value={infoData['price']} onChange={(e) => this.dataChange(e.target.value, 'price')} />
                         </div>
                         <div className="product_amendInput">
                             <label htmlFor="state" className="product_type">是否在售：</label>
-                            <Radio.Group onChange={props.radioChange} value={props.editInfoData['state']}>
+                            <Radio.Group onChange={(e) => this.dataChange(e.target.value, 'state')} value={infoData['state']}>
                                 <Radio value={1}>在售</Radio>
                                 <Radio value={0}>下架</Radio>
                             </Radio.Group>
                         </div>
                         <Fragment>
-                            <Button htmlType="submit" className="product_sure" onClick={props.changeSure}>确定</Button>
-                            <Button htmlType="submit" className="product_sure" onClick={props.changeCancel}>取消</Button>
+                            <Button htmlType="submit" className="product_sure" onClick={changeSure}>确定</Button>
+                            <Button htmlType="submit" className="product_sure" onClick={changeCancel}>取消</Button>
                         </Fragment>
                     </div>
-                    :
-                    <div className="product_form">
-                        <div className="product_amendInput">
-                            <label htmlFor="price" className="product_type">商品信息：</label>
-                            <Input placeholder="name" id="price" onChange={props.nameChange} />
-                        </div>
-                        <div className="product_amendInput">
-                            <label htmlFor="price" className="product_type">商品价格：</label>
-                            <Input placeholder="price" id="price" onChange={props.priceChange} />
-                        </div>
-                        <div className="product_amendInput">
-                            <label htmlFor="state" className="product_type">是否在售：</label>
-                            <Radio.Group onChange={props.radioChange} value={props.editInfoData['state']}>
-                                <Radio value={1}>在售</Radio>
-                                <Radio value={0}>下架</Radio>
-                            </Radio.Group>
-                        </div>
-                        <Fragment>
-                            <Button htmlType="submit" className="product_sure" onClick={props.changeSure}>确定</Button>
-                            <Button htmlType="submit" className="product_sure" onClick={props.changeCancel}>取消</Button>
-                        </Fragment>
-                    </div>
-            }
-        </div>
-    )
+                }
+            </div>
+        )
+    }
 }
+
 
 // 类型检查
 EditInfo.propTypes = {
--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,237 +1,229 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, Component } from 'react';
 import EditInfo from './components/editInfo';
 import { Select, Input, Button, Icon, Table, Popconfirm, message } from 'antd';
 import './index.less';
 const { Option } = Select;
 
-const ProductPage = () => {
-
-    const options = [
-        { value: '0', title: '商品序列号' },
-        { value: '1', title: '商品信息' },
-    ]
-
-    //记录查询（options）
-    const [selectOption, setOption] = useState(0);
-
-    //记录查询（Input值）
-    const [selectInput, setInput] = useState('');
-
-    //查询
-    const searchListData = () => {
-        const selectInputData = selectOption === 0 ? listData.filter(val => val.id === Number(selectInput)) : listData.filter(val => val.name === selectInput);
-        setListData([...selectInputData]);
-    }
-
-    //table的header内容
-    const [columns] = useState([
-        {
-            title: '序列号',
-            dataIndex: 'id',
-            width: '20%'
-        },
-        {
-            title: '商品信息',
-            dataIndex: 'name',
-            width: '20%'
-        },
-        {
-            title: '价格(元)',
-            dataIndex: 'price',
-            width: '20%'
-        },
-        {
-            title: '状态',
-            dataIndex: 'state',
-            width: '20%',
-            render: (states, record, index) => {
-                return (
-                    <span>{states === 0 ? '已下架' : ' 在售'}</span>
-                )
-            }
-        },
-        {
-            title: '操作',
-            dataIndex: 'tool',
-            width: '20%',
-            render: (tools, record, index) => {
-                return (
-                    <div>
-                        {
-                            tools.map((item, key) => {
-                                if (key === 0) {
-                                    return (<Button ghost key={item} type='primary' style={{ marginRight: '10px' }} onClick={() => changeTable('edit', index)}>{item}</Button>)
-                                } else {
-                                    return (
-                                        <Popconfirm key={item} title="确定删除?" cancelText="取消" okText="确定" onConfirm={() => handleDelete(index)}>
-                                            <Button ghost type='danger' style={{ marginRight: '10px' }}>{item}</Button>
-                                        </Popconfirm>
-                                    )
+class ProductPage extends Component {
+    
+    constructor(props) {
+        super(props)
+        this.state = {
+            options: [
+                { value: '0', title: '商品序列号' },
+                { value: '1', title: '商品信息' }
+            ],
+            selectOption: 0,//记录查询（options）
+            selectInput: '',//记录查询（Input值）
+            columns: [  //table的header内容
+                {
+                    title: '序列号',
+                    dataIndex: 'id',
+                    width: '20%'
+                },
+                {
+                    title: '商品信息',
+                    dataIndex: 'name',
+                    width: '20%'
+                },
+                {
+                    title: '价格(元)',
+                    dataIndex: 'price',
+                    width: '20%'
+                },
+                {
+                    title: '状态',
+                    dataIndex: 'state',
+                    width: '20%',
+                    render: (states, record, index) => {
+                        return (
+                            <span>{states === 0 ? '已下架' : ' 在售'}</span>
+                        )
+                    }
+                },
+                {
+                    title: '操作',
+                    dataIndex: 'tool',
+                    width: '20%',
+                    render: (tools, record, index) => {
+                        return (
+                            <div>
+                                {
+                                    tools.map((item, key) => {
+                                        if (key === 0) {
+                                            return (<Button ghost key={item} type='primary' style={{ marginRight: '10px' }} onClick={() => this.changeTable('edit', index)}>{item}</Button>)
+                                        } else {
+                                            return (
+                                                <Popconfirm key={item} title="确定删除?" cancelText="取消" okText="确定" onConfirm={() => this.handleDelete(index)}>
+                                                    <Button ghost type='danger' style={{ marginRight: '10px' }}>{item}</Button>
+                                                </Popconfirm>
+                                            )
+                                        }
+                                    })
                                 }
-                            })
-                        }
-                    </div>
-                )
-            }
-        },
-    ])
-
-    //列表数据
-    const [listData, setListData] = useState([
-        {
-            key: '1',
-            id: Math.round(Math.random() * 10000),
-            name: '番茄',
-            price: parseFloat(Math.random() * 100).toFixed(2),
-            state: 1,
-            tool: ['修改', '删除'],
-        },
-        {
-            key: '2',
-            id: Math.round(Math.random() * 10000),
-            name: '番薯叶',
-            price: parseFloat(Math.random() * 100).toFixed(2),
-            state: 1,
-            tool: ['修改', '删除'],
-        },
-        {
-            key: '3',
-            id: Math.round(Math.random() * 10000),
-            name: '番茄',
-            price: parseFloat(Math.random() * 100).toFixed(2),
-            state: 1,
-            tool: ['修改', '删除'],
-        },
-        {
-            key: '4',
-            id: Math.round(Math.random() * 10000),
-            name: '番薯叶',
-            price: parseFloat(Math.random() * 100).toFixed(2),
-            state: 1,
-            tool: ['修改', '删除'],
-        },
-        {
-            key: '5',
-            id: Math.round(Math.random() * 10000),
-            name: '番茄',
-            price: parseFloat(Math.random() * 100).toFixed(2),
-            state: 1,
-            tool: ['修改', '删除'],
-        },
-        {
-            key: '6',
-            id: Math.round(Math.random() * 10000),
-            name: '番薯叶',
-            price: parseFloat(Math.random() * 100).toFixed(2),
-            state: 1,
-            tool: ['修改', '删除'],
+                            </div>
+                        )
+                    }
+                },
+            ],
+            listData: [//列表数据
+                {
+                    key: '1',
+                    id: Math.round(Math.random() * 10000),
+                    name: '番茄',
+                    price: parseFloat(Math.random() * 100).toFixed(2),
+                    state: 1,
+                    tool: ['修改', '删除'],
+                },
+                {
+                    key: '2',
+                    id: Math.round(Math.random() * 10000),
+                    name: '番薯叶',
+                    price: parseFloat(Math.random() * 100).toFixed(2),
+                    state: 1,
+                    tool: ['修改', '删除'],
+                },
+                {
+                    key: '3',
+                    id: Math.round(Math.random() * 10000),
+                    name: '番茄',
+                    price: parseFloat(Math.random() * 100).toFixed(2),
+                    state: 1,
+                    tool: ['修改', '删除'],
+                },
+                {
+                    key: '4',
+                    id: Math.round(Math.random() * 10000),
+                    name: '番薯叶',
+                    price: parseFloat(Math.random() * 100).toFixed(2),
+                    state: 1,
+                    tool: ['修改', '删除'],
+                },
+                {
+                    key: '5',
+                    id: Math.round(Math.random() * 10000),
+                    name: '番茄',
+                    price: parseFloat(Math.random() * 100).toFixed(2),
+                    state: 1,
+                    tool: ['修改', '删除'],
+                },
+                {
+                    key: '6',
+                    id: Math.round(Math.random() * 10000),
+                    name: '番薯叶',
+                    price: parseFloat(Math.random() * 100).toFixed(2),
+                    state: 1,
+                    tool: ['修改', '删除'],
+                }
+            ],
+            dataIndex: null,//记录弹出编辑器的列表下标
+            isShowChange: false,//是否弹出修改编辑器状态
+            infoData: {},//编辑的商品信息
+            editType: 'edit',//记录弹出编辑器是修改还是添加
         }
-    ])
-
-    //记录弹出编辑器的列表下标
-    const [dataIndex, setDataIndex] = useState(null);
-
-    //是否弹出修改编辑器状态
-    const [isShowChange, setIsShowChange] = useState(false);
-
-    //编辑的商品信息
-    let [editInfoData, setEditInfoData] = useState({})
-
-    //删除列表数据某一项
-    const handleDelete = (index) => {
-        listData.splice(index, 1);
-        setListData([...listData]);
-        console.log(listData)
     }
-
-    const [editType, setEditType] = useState('edit');
 
     //弹出修改编辑器
-    const changeTable = (type, index) => {
-        setIsShowChange(true);
-        if (type === 'edit') {
-            setDataIndex(index);
-            console.log(listData[index]);
-            setEditInfoData((prevData) => ({ ...prevData, ...listData[index] }))
-        };
+    changeTable(type, index) {
+        this.setState({
+            editType: type,
+            dataIndex: index,
+            isShowChange: true,
+            infoData: type === 'edit' ? this.state.listData[index] : {}
+        });
     }
 
-    //修改编辑器（监听添加name事件）
-    const nameChange = (e) => {
-        setEditInfoData(prevData => ({ ...prevData, ...{ name: e.target.value } }));
-    }
-
-    //修改编辑器（监听price事件）
-    const priceChange = (e) => {
-        setEditInfoData(prevData => ({ ...prevData, ...{ price: e.target.value } }));
-    }
-
-    //修改编辑器（改变是否在售事件）
-    const radioChange = (e) => {
-        setEditInfoData(prevData => ({ ...prevData, ...{ state: e.target.value } }));
+    //查询
+    searchListData() {
+        let { selectOption, selectInput, listData } = this.state;
+        const selectInputData = selectOption === 0 ? listData.filter(val => val.id === Number(selectInput)) : listData.filter(val => val.name === selectInput);
+        this.setState({
+            listData: selectInputData
+        });
     }
 
     //修改编辑器（确定事件）
-    const changeSure = () => {
-        setIsShowChange(false);
-        if (editType === 'edit') {
-            if (editInfoData['price'] !== listData[dataIndex]['price'] || editInfoData['state'] !== listData[dataIndex]['state']) {
-                const editList = JSON.parse(JSON.stringify(listData));
-                editList.splice(dataIndex, 1, editInfoData);
+    changeSure() {
+        let { dataIndex, editType, infoData, listData } = this.state;
+        this.setState(() => ({ isShowChange: false }));
 
-                setListData((prevData => [...prevData]));
+        //编辑列表
+        if (editType === 'edit') {
+            if ((infoData['price'] && infoData['price'] !== listData[dataIndex]['price']) || infoData['state'] !== listData[dataIndex]['state']) {
+                let editListData = JSON.parse(JSON.stringify(listData));
+                editListData.splice(dataIndex, 1, infoData);
+                this.setState(() => ({ listData: editListData }));
                 message.success('修改成功');
+            } else {
+                message.error('修改失败');
             }
         } else {
-            console.log('我来添加的')
-            // if (editInfoData['name'] !== '' && editInfoData['price'] !== '') {
-            //     Object.assign({ ...editInfoData }, { key: listData.length + 1 });
-            //     console.log(editInfoData);
-            //     message.success('添加成功');
-            //     // listData.push(editInfoData);
-            //     // setListData([...listData])
-            // } else {
-            //     message.error('添加失败，缺少参数')
-            // }
+            //增加列表
+            if (infoData['name'] !== '' && infoData['price'] !== '' && infoData['state']) {
+                let [editListData, editInfoData] = [JSON.parse(JSON.stringify(listData)), JSON.parse(JSON.stringify(infoData))];
+                //获取列表的key的最大值
+                editListData.sort((a, b) => Number(b['key']) - Number(a['key']));
+                const isMaxKey = Number(editListData[0]['key']) + 1;
+                Object.assign(editInfoData, { key: String(isMaxKey), id: 5248, tool: ['修改', '删除'] });
+                editListData.push(editInfoData);
+                this.setState(() => ({ listData: editListData }));
+                message.success('添加成功');
+            } else {
+                message.error('添加失败，缺少参数')
+            }
         }
     }
 
-    //修改编辑器（取消事件）
-    const changeCancel = () => {
-        setIsShowChange(false);
+    //删除列表数据某一项
+    handleDelete(index) {
+        let editListData = JSON.parse(JSON.stringify(this.state.listData));
+        editListData.splice(index, 1);
+        this.setState({
+            listData: editListData
+        });
     }
 
-    return (
-        <Fragment>
-            <div className="product_box">
-                <div className="product_header">
-                    <div className="product_tool">
-                        <Select className="product_select" defaultValue={options[0]['value']} onChange={(value) => setOption(value)}>
-                            {
-                                options.map(item => {
-                                    return (
-                                        <Option value={item['value']} key={item['title']}>{item['title']}</Option>
-                                    )
-                                })
-                            }
-                        </Select>
-                        <Input className="product_input" placeholder="关键词" onChange={(e) => setInput(e.target.value)}></Input>
-                        <Button onClick={searchListData}>查询</Button>
+    render() {
+        let { options, listData, columns, isShowChange, editType, infoData } = this.state;
+        return (
+            <Fragment>
+                <div className="product_box">
+                    <div className="product_header">
+                        <div className="product_tool">
+                            <Select className="product_select" defaultValue={options[0]['value']} onChange={(value) => this.setState({ selectOption: value })}>
+                                {
+                                    options.map(item => {
+                                        return (
+                                            <Option value={item['value']} key={item['title']}>{item['title']}</Option>
+                                        )
+                                    })
+                                }
+                            </Select>
+                            <Input className="product_input" placeholder="关键词" onChange={(e) => this.setState({ selectInput: e.target.value })}></Input>
+                            <Button onClick={this.searchListData.bind(this)}>查询</Button>
+                        </div>
+                        <Button className="product_add" type="primary" onClick={() => this.changeTable('add')}>
+                            <Icon type="plus"></Icon>
+                            <div className="product_addBtm">添加商品</div>
+                        </Button>
                     </div>
-                    <Button className="product_add" type="primary" onClick={() => changeTable('add')}>
-                        <Icon type="plus"></Icon>
-                        <div className="product_addBtm">添加商品</div>
-                    </Button>
+
+                    <div className="product_table">
+                        <Table bordered dataSource={listData} columns={columns} pagination={false} />
+                    </div>
                 </div>
 
-                <div className="product_table">
-                    <Table bordered dataSource={listData} columns={columns} pagination={false} />
-                </div>
-            </div>
-
-            <EditInfo isShowChange={isShowChange} editType={editType} nameChange={nameChange} editInfoData={editInfoData} radioChange={radioChange} priceChange={priceChange} changeSure={changeSure} changeCancel={changeCancel} />
-        </Fragment>
-    )
+                <EditInfo
+                    isShowChange={isShowChange}
+                    editType={editType}
+                    infoData={infoData}
+                    changeSure={this.changeSure.bind(this)}
+                    editDataChange={(event) => this.setState(() => ({ infoData: event }))}
+                    changeCancel={() => this.setState(() => ({ isShowChange: false }))}
+                ></EditInfo>
+            </Fragment>
+        )
+    }
 }
 
 export default ProductPage;
